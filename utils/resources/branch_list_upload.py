7f324bcac08b18955538f19dbbd9d4140979ff8a
Rosemary Rajan <rosemarymannattu@gmail.com>
2021-02-18 14:31:22 +0530

    restore db

--- utils/resources/branch_list_upload.py
+++ utils/resources/branch_list_upload.py
@@ -6,15 +6,20 @@ from apps.banks.models import Bank, Branch
 def create_bank_and_branch():
     with open('utils/resources/bank_branches.csv', 'r') as file:
         reader = csv.DictReader(file)
+        count = 0
+
         for row in reader:
             try:
-                bank = Bank.objects.get(id=row['bank_id'])
-                branch = Branch.objects.get(ifsc=row['ifsc'], bank=bank)
+                if not count > 80000:
+                    bank = Bank.objects.get(id=row['bank_id'])
+                    branch = Branch.objects.get(ifsc=row['ifsc'], bank=bank)
 
             except Branch.DoesNotExist:
                 Branch.objects.create(bank=bank, ifsc=row['ifsc'], branch=row['branch'], address=row['address'],
                                       city=row['city'], district=row['district'], state=['state'])
+
             except Bank.DoesNotExist:
                 bank = Bank.objects.create(id=row['bank_id'], name=row['bank_name'])
                 Branch.objects.create(bank=bank, ifsc=row['ifsc'], branch=row['branch'], address=row['address'],
                                       city=row['city'], district=row['district'], state=['state'])
+            count += 1
